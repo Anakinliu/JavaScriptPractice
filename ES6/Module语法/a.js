85f09ceb37301a75ed06a7fb26d2a8600c0d4d91
Anakinliu <gugeliuyinquan@gmail.com>
2021-05-18 10:04:32 +0800

    删除node模块安装产生的文件

---
+++ ES6/Module语法/a.js
@@ -0,0 +1,11 @@
+var a = 1
+var arr = [1, 2, 3]
+var f = function () {
+    console.log(233)
+}
+
+export {
+    a,
+    arr,
+    f
+}
---
+++ ES6/Module语法/b.js
@@ -0,0 +1,9 @@
+import {
+    a,
+    arr,
+    f
+} from './a.js'
+
+console.log(a);
+console.log(arr);
+f();
